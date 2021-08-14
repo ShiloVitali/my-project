f730af49f89a87412b08bc9e52aa8a2e0b3131c7
ShiloVitali <shilo.vitali@gmail.com>
2021-08-14 17:53:57 +0300

    git

--- src/components/filter/MainFilter.js
+++ src/components/filter/MainFilter.js
@@ -5,9 +5,8 @@ function MainFilter({route, country, region}) {
   return(
     <div className="row pb-3">
       <div className="col-3 p-4 my-auto text-secondary"><span>{route}</span></div>
-      <div className="col-9 pe-3">
+      <div className="col-9 pe-3 my-auto">
       <SectionFilter country={country}/>
-      <SectionFilter country={region}/>
       </div>
     </div>
   );
